90e608c68b026755c5101bd891d321135fb3ef3a
TOMAS <guth.tomas@seznam.cz>
2017-10-27 13:57:29 +0200

    News Api Reader implemented

--- app/src/main/java/com/example/recyclerview/Api/RetrofitHelper.java
+++ app/src/main/java/com/example/recyclerview/Api/RetrofitHelper.java
@@ -1,16 +1,9 @@
 package com.example.recyclerview.Api;
 
-import android.content.Context;
-import android.util.Log;
-import android.widget.Toast;
-
 import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-import static android.content.ContentValues.TAG;
 import static com.example.recyclerview.MainActivity.API_URL;
 
 /**
@@ -18,36 +11,38 @@ import static com.example.recyclerview.MainActivity.API_URL;
  */
 
 public class RetrofitHelper {
-    Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl(API_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
 
-    String APIKEY = "6088427f16e14eb1923a64828952d8aa";
-    NewsJson allTheNews;
+    private static RetrofitHelper instance = null;
+    private String APIKEY = "6088427f16e14eb1923a64828952d8aa";
+    private NewsJson allTheNews;
+    private NewsImporter hlasatel;
+    private Retrofit retrofit;
 
 
-    public void readArticles(final Context context) {
-        NewsImporter hlasatel = retrofit.create(NewsImporter.class);
-        Call<NewsJson> call = hlasatel.news_article("techcrunch",APIKEY);
+    private RetrofitHelper() {
+        retrofit = new Retrofit.Builder()
+                .baseUrl(API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        hlasatel = retrofit.create(NewsImporter.class);
+    }
 
-        call.enqueue(new Callback<NewsJson>() {
-            @Override
-            public void onResponse(Call<NewsJson> call, Response<NewsJson> response) {
-                allTheNews = new NewsJson(response.body());
-                if (allTheNews== null) {
-                    return;
-                }
-                Toast.makeText(context, allTheNews.source, Toast.LENGTH_LONG).show();
-            }
+    public static RetrofitHelper getInstance() {
+        if (instance == null) {
+            return new  RetrofitHelper();
+        }
+        else
+        {
+            return instance;
+        }
+    }
 
-            @Override
-            public void onFailure(Call<NewsJson> call, Throwable t) {
-                Log.e(TAG, "response error", t);
-                Toast.makeText(context, "chyba bracho", Toast.LENGTH_LONG).show();
-            }
-        });
+    public NewsImporter getNewsImporter() {
+        return hlasatel;
+    }
 
+    public Call<NewsJson> getNewsCall(String source) {
+        return hlasatel.news_article(source,APIKEY);
     }
 
     public NewsJson getAllTheNews() {
--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.recyclerview.Api.NewsJson;
 import com.example.recyclerview.Api.RetrofitHelper;
@@ -11,6 +13,10 @@ import com.example.recyclerview.RecyclerViewStuff.FeedItem;
 import com.example.recyclerview.RecyclerViewStuff.MyAdapter;
 
 import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class MainActivity extends Activity {
@@ -20,29 +26,51 @@ public class MainActivity extends Activity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     public static final String API_URL = "https://newsapi.org/";
+    private ArrayList<FeedItem> mArticleList = new ArrayList<>();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-
-        RetrofitHelper mujPomocnicek = new RetrofitHelper();
-        mujPomocnicek.readArticles(this);
 
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        mAdapter = new MyAdapter(createDataset(mujPomocnicek.getAllTheNews()));
+        mAdapter = new MyAdapter(mArticleList);
         mRecyclerView.setAdapter(mAdapter);
+
+        startNetworkRequest("techcrunch");
+
+    }
+
+    private void startNetworkRequest(String articleSource) {
+        Call<NewsJson> call = RetrofitHelper.getInstance().getNewsCall(articleSource);
+        call.enqueue(new Callback<NewsJson>() {
+            @Override
+            public void onResponse(Call<NewsJson> call, Response<NewsJson> response) {
+                NewsJson allTheNews = new NewsJson(response.body());
+                Toast.makeText(MainActivity.this, allTheNews.getSource(), Toast.LENGTH_LONG).show();
+
+                mArticleList.addAll(createDataset(allTheNews));
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<NewsJson> call, Throwable t) {
+                Log.e(TAG, "response error", t);
+                Toast.makeText(MainActivity.this, "chyba bracho", Toast.LENGTH_LONG).show();
+            }
+
+        });
+
     }
 
     private ArrayList<FeedItem> createDataset(NewsJson json) {
 
-        ArrayList<Article> articles = json.getArticles(); // Tady to háže null exception, protože prý getArticles se volá na null referenci.
+        ArrayList<Article> articles = json.getArticles();
         Article articleNow;
         ArrayList<FeedItem> myDataset = new ArrayList<>();
 
@@ -50,6 +78,7 @@ public class MainActivity extends Activity {
             articleNow = articles.get(i);
             myDataset.add(new FeedItem(articleNow.getTitle(),articleNow.getDescription(),articleNow.getUrlToImage(),articleNow.getTitle()));
         }
+
         return myDataset;
     }
 }
