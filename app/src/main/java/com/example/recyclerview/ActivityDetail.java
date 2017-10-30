32fa5dd5d03763d5f998a5d645f9723a6eae25ef
TOMAS <guth.tomas@seznam.cz>
2017-10-30 11:22:46 +0100

    Detail - WIP

---
+++ app/src/main/java/com/example/recyclerview/ActivityDetail.java
@@ -0,0 +1,13 @@
+package com.example.recyclerview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ActivityDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
